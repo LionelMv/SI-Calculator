35404bc59af822a3d8dcb2db2b5ba4df457d47ad
LionelMv <mvangicode@gmail.com>
2025-11-13 17:47:16 +0300

    feat: Change instruments.html to use instruments api

--- frontend_ui/views.py
+++ frontend_ui/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from calculator_api.constants import CHOICES, INSTRUMENTS
+from rest_framework.request import Request
+from django.http import HttpResponse
+# from calculator_api.constants import CHOICES, INSTRUMENTS
 
 
-def index(request):
-    instruments_map = {choice[1]: INSTRUMENTS.get(choice[0])
-                    for choice in CHOICES}
+# def index(request):
+#     instruments_map = {choice[1]: INSTRUMENTS.get(choice[0])
+#                     for choice in CHOICES}
 
-    return render(request, 'frontend_ui/index.html', {
-        'instruments': CHOICES,
-        'instruments_map': instruments_map
-    })
+#     return render(request, 'frontend_ui/index.html', {
+#         'instruments': CHOICES,
+#         'instruments_map': instruments_map
+#     })
+
+def index(request: Request) -> HttpResponse:
+    return render(request, 'frontend_ui/index.html')
